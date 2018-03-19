a6c28b0ef2ba4c707c6ca80be5ccd51db6597a04
Alan Zhu <alan.zhu39@gmail.com>
2018-03-19 09:30:36 -0700

    Finished Unit12

--- Unit12_18e/src/Lab18e.java
+++ Unit12_18e/src/Lab18e.java
@@ -15,16 +15,31 @@ public class Lab18e
 {
 	public static void main( String args[] ) throws IOException
 	{
-		
-
-
-
-
-
-
-
-
-
-
+		Scanner file = new Scanner(new File("/Users/alanzhu39/Desktop/AlanZhuAPCSA/Unit12_18e/src/lab18e.dat"));
+		int size = file.nextInt();
+		Word[] words = new Word[size];
+		for(int index = 0; index < size; index++)
+		{
+			Word temp = new Word(file.next());
+			words[index] = temp;
+		}
+		for(int index = 0; index < words.length - 1; index++)
+		{
+			int minIndex = index;
+			for(int next = index + 1; next < words.length; next++)
+			{
+				if(words[next].compareTo(words[minIndex]) <= 0)
+				{
+					minIndex = next;
+				}
+			}
+			Word temp = words[index];
+			words[index] = words[minIndex];
+			words[minIndex] = temp;
+		}
+		for(Word word : words)
+		{
+			System.out.println(word);
+		}
 	}
 }
